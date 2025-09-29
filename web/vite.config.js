6d1e4c66c6a0cca5b87372de369324695cc618c6
chenxiujian-ggg <13226011065@163.com>
2025-09-29 16:57:02 +0800

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -25,6 +25,7 @@ const { vitePluginSemi } = pkg;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
